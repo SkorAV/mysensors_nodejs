7d0c85923cdfa38489a651be66e0c26901808428
Andrii.S <skor_av@ukr.net>
2020-01-13 20:19:31 +0200

    Fixing the baud rate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const SerialPort = require('serialport');
 const ReadLine = require('@serialport/parser-readline');
 const portPath = '/dev/ttyUSB100';
-const port = new SerialPort(portPath, { baudRate: 115600 });
+const port = new SerialPort(portPath, { baudRate: 115200 });
 
 const parser = new ReadLine();
 port.pipe(parser);
