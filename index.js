2c733e7da36d9d2a5026f3ec69f8cb159c7a00ea
Andrii.S <skor_av@ukr.net>
2020-01-13 19:50:39 +0200

    Reverted the port changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SerialPort = require('serialport');
 const ReadLine = require('@serialport/parser-readline');
-const portPath = '/dev/pts/0';
+const portPath = '/dev/ttyUSB100';
 const port = new SerialPort(portPath, { baudRate: 115600 });
 
 const parser = new ReadLine();
